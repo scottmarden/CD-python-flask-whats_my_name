890f7d68941162f1fc742550662ae97e7aef8cb0
Scott <marden.scott@gmail.com>
2017-06-08 15:51:28 -0700

    Finished assignment. Focus was on form submission and redirect in a route

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def submit_form():
+	print request.form['name']
+	return redirect('/')
+
+
+app.run(debug=True)
